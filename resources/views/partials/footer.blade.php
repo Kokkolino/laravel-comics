0dc1455bc25afd972f302145dca3d5ce3ce8a3e4
Francesco C <fran461j@outlook.it>
2023-01-12 23:34:59 +0100

    footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,187 @@
+{{-- blue badge --}}
+<section id="badge" class="bg-blue">
+    <div class="w-70 ">
+        {{-- ul badge --}}
+        <ul class="flex jc-bw al-center">
+            <li>
+                <a href="">
+                    <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="">
+                    <span class="color-white">
+                        DIGITAL COMICS
+                    </span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="{{asset('img/buy-comics-merchandise.png')}}" alt="">
+                    <span class="color-white">
+                        DC MERCHANDISE
+                    </span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="{{asset('img/buy-comics-subscriptions.png')}}" alt="">
+                    <span class="color-white">
+                        SUBSCRIPTION
+                    </span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="{{asset('img/buy-comics-shop-locator.png')}}" alt="">
+                    <span class="color-white">
+                        COMIC SHOP LOCATOR
+                    </span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="{{asset('img/buy-dc-power-visa.svg')}}" alt="">
+                    <span class="color-white">
+                        DC POWER VISA
+                    </span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</section>
+<footer>
+    {{-- faqs section --}}
+    <section id="faq-bg" class="flex al-center">
+        <div class="w-70 flex jc-bw">
+            {{-- faqs --}}
+            <div class="flex" style="gap: 25px"> 
+                {{-- colonna 1 --}}
+                <ul class="faq">
+                    {{-- elem 1 col 1 --}}
+                    <ul>
+                        <li>
+                            <h3>
+                                DC COMICS
+                            </h3>
+                        </li>
+                        <li>
+                            <a href="">Characters</a>
+                        </li>
+                        <li>
+                            <a href="">Comics</a>
+                        </li>
+                        <li>
+                            <a href="">Movies</a>
+                        </li>
+                        <li>
+                            <a href="">TV</a>
+                        </li>
+                        <li>
+                            <a href="">Games</a>
+                        </li>
+                        <li>
+                            <a href="">Video</a>
+                        </li>
+                        <li>
+                            <a href="">News</a>
+                        </li>
+                    </ul>
+                    {{-- elem 2 col 1 --}}
+                    <ul>
+                        <li>
+                            <h3>SHOP</h3>
+                        </li>
+                        <li>
+                            <a href="">Shop DC</a>
+                        </li>
+                        <li>
+                            <a href="">Shop DC Collectibles</a>
+                        </li>
+
+                    </ul>
+                </ul>
+                {{-- colonna 2 --}}
+                <ul class="faq">
+                    <li>
+                        <h3>DC</h3>
+                    </li>
+                    <li>
+                        <a href="">Terms of Use</a>
+                    </li>
+                    <li>
+                        <a href="">Privacy policy (new)</a>
+                    </li>
+                    <li>
+                        <a href="">Ad Choices</a>
+                    </li>
+                    <li>
+                        <a href="">Advertising</a>
+                    </li>
+                    <li>
+                        <a href="">Jobs</a>
+                    </li>
+                    <li>
+                        <a href="">Subscriptions</a>
+                    </li>
+                    <li>
+                        <a href="">Talent Workshops</a>
+                    </li>
+                    <li>
+                        <a href="">CPSC Certificates</a>
+                    </li>
+                    <li>
+                        <a href="">Ratings</a>
+                    </li>
+                    <li>
+                        <a href="">Shop Help</a>
+                    </li>
+                    <li>
+                        <a href="">Contact Us</a>
+                    </li>
+                </ul>
+                {{-- colonna 3 --}}
+                <ul class="faq">
+                    <li>
+                        <h3>SITES</h3>
+                    </li>
+                    <li>
+                        <a href="">DC</a>
+                    </li>
+                    <li>
+                        <a href="">MAD Magazine</a>
+                    </li>
+                    <li>
+                        <a href="">DC Kids</a>
+                    </li>
+                    <li>
+                        <a href="">DC Universe</a>
+                    </li>
+                    <li>
+                        <a href="">DC Power Visa</a>
+                    </li>
+
+                </ul>
+            </div>
+            {{-- logo --}}
+            <div id="footer-logo">
+                <div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    {{-- sign up --}}
+    <section id="sign-up" >
+        <div class="w-70 flex jc-bw al-center">
+            <button class="color-white">
+                <h1>SIGN UP NOW!</h1>
+            </button>
+
+            <div class="flex" style="gap: 10px">
+                <a href=""><h1>FOLLOW US</h1></a>
+                <a href=""><img src="{{asset('img/footer-facebook.png')}}" alt=""></a>
+                <a href=""><img src="{{asset('img/footer-twitter.png')}}" alt=""></a>
+                <a href=""><img src="{{asset('img/footer-youtube.png')}}" alt=""></a>
+                <a href=""><img src="{{asset('img/footer-pinterest.png')}}" alt=""></a>
+                <a href=""><img src="{{asset('img/footer-periscope.png')}}" alt=""></a>
+            </div>
+        </div>
+    </section>
+</footer>
